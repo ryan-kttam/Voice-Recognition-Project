f08485445ec14cdebe7ad1d46a67448a9ef2fb88
ryan-kttam <ryantam0626@gmail.com>
2017-11-20 21:29:41 -0800

    finalize ML analysis - version 1

--- voice_rocognition.py
+++ voice_rocognition.py
@@ -2,8 +2,10 @@
 # Support Vector Machine
 # outline:
 # 1. data exploration
-# 2. data standardization
-# 3.
+# 2. data standardization and splitting data
+# 3. Model training with default parameters
+# 4. Model tuning
+# 5. Test the final model with the test set
 
 # Version 1
 
@@ -39,8 +41,11 @@ label = data['label']
 features = data.drop('label', axis = 1)
 features.head()
 
-# 2. data Standardization
-#standardizing the data. (so that each attribute will have zero mean and sd of 1)
+# 2. Data Standardization
+# Data Standardization is a good practice to neutralize variables that have large values.
+# Standardizing the data allows our training model to treat each features equally.
+# standardizing the data using StandardScaler
+# each attribute will have mean of 0 and sd of 1
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 features = scaler.fit_transform(features)
@@ -48,95 +53,100 @@ features = scaler.fit_transform(features)
 # scaler.fit(features)
 # features = scaler.transform(features)
 
-# split the data into training set and test set,
+# split the data into training set and test set
 from sklearn.model_selection import train_test_split
-x_train , x_test, y_train, y_test = train_test_split( features, label, test_size=0.1 , random_state = 1 )
+x_train, x_test, y_train, y_test = train_test_split(features, label, test_size = 0.1, random_state = 1 )
 
-# Support Vector Machine
-from sklearn.svm import SVC
+# 3. Model training using Support Vector Machine with default parameters
 from sklearn.model_selection import cross_val_score, KFold
-from sklearn.metrics import make_scorer, accuracy_score
+from sklearn.metrics import  accuracy_score , make_scorer
+from sklearn.svm import SVC
+clf = SVC()
+clf.fit(x_train, y_train)
+predictions = clf.predict(x_test)
+accuracy_score(y_test, predictions)
+# With default parameter, the accuracy on the test set is is 96.21%
 
-# finding the best C when kernel = 'rbf'
-C_range = list(range(1,100))
+
+# 4. Model tuning
+# We will stick with kernel = 'rbf', and tune 'C' and 'gamma'
+# starting with 'C'
+# with C from 1 - 100
+C_range = list(range(1, 100))
 c_tuning_score = []
-k_fold = KFold(n_splits=10, random_state=1,shuffle=True) # object for 10-fold cv
+k_fold = KFold(n_splits=10, random_state=1, shuffle=True) # object for 10-fold cv
 scorer = make_scorer(accuracy_score) # scorer for cv, accuracy_score in this case
 for c in C_range:
-   model = SVC(C = c)
+   model = SVC(C=c)
    cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
    c_tuning_score.append(cv_scores.mean())
-# plot the c_tuning_scores
+
+# visualize and observe which 'C' give us the best accuracy
 import matplotlib.pyplot as plt
-plt.plot(range(1,101),c_tuning_score)
+plt.plot(range(1, 100), c_tuning_score)
 plt.ylabel('cross-validated accuracy')
-# we can see that the maximum is around C = 5-15,
-plt.plot(range(1,16),c_tuning_score[1:16])
-# in the above plot we can see that the maximum of C is 10.
-# Let's break down 10 and see what is the exact value for C will be best for our prediction.
-C_range2 = np.arange(9.0,11.0,0.1)
+plt.xlabel('parameter: C')
+# The maximum for C is around between 5 to 15,
+plt.plot(range(5, 15), c_tuning_score[5:15])
+# in the above plot we can see that the maximum of C is from 9 to 11.
+# Let's break down the 10 and explore the best value for C.
+C_range2 = list ( np.arange(9.00, 11, 0.01) )
 c_tuning_score2 = []
 for c in C_range2:
-   model = SVC(C = c)
-   cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
-   c_tuning_score2.append(cv_scores.mean())
-plt.plot(np.arange(9.0, 11.0, 0.1), c_tuning_score2)
-# C = 10 is th best
-# next is to find the best g.
-g_range = [0.0001,0.001,.01,.1,1,10,100]
+   model = SVC(C=c)
+   cv_scores2 = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
+   c_tuning_score2.append(cv_scores2.mean())
+plt.plot(np.arange(9.00, 11.00, 0.01), c_tuning_score2)
+# C = 10 is the best
+
+# Next we are going to find the best gamma.
+# lets start from 0.0001 to 100
+g_range = [0.0001, 0.001, .01, .1, 1, 10, 100]
 g_tuning_score = []
 for g in g_range:
    model = SVC(gamma= g, C=10)
    cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
    g_tuning_score.append(cv_scores.mean())
 plt.plot(g_range, g_tuning_score)
-# gamma significantly decreases its performance when it is > 10, so we zoom in and see which performed the best
-plt.plot(g_range[1:5], g_tuning_score[1:5])
+# Gamma significantly decreases its performance when it is > 10,
+# so we zoom in and see which performed the best
+plt.plot(g_range[0:5], g_tuning_score[0:5])
 # As we can see from the plot, gamma peak at 0.1.
 # our next step is to find the exact value of gamma, by zooming in even more.
-g_range2 = np.arange(0.01,0.5,0.01)
+g_range2 = np.arange(0.01, 0.2, 0.01)
 g_tuning_score2 = []
 for g in g_range2:
    model = SVC(gamma= g, C=10)
    cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
    g_tuning_score2.append(cv_scores.mean())
 plt.plot(g_range2, g_tuning_score2)
-# we can see that gamma peak at around 0.2 - 0.3, we need to zoom to get the exact value
-g_range3 = np.arange(0.02,0.1,0.001)
-g_tuning_score3 = []
-for g in g_range3:
-   model = SVC(gamma= g, C=10)
-   cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
-   g_tuning_score3.append(cv_scores.mean())
-plt.plot(g_range3, g_tuning_score3)
-# zoom in even more
-plt.plot(g_range3[30:40], g_tuning_score3[30:40])
-# the best gamma we have got is 0.055
+# the best gamma we have got is 0.05
 
-# plug in the best parameter: C = 10 and gamma = 0.055,
-clf = SVC(C=10, gamma = 0.055)
-k_fold = KFold(n_splits=10, random_state=1,shuffle=True)
+# 5. Test the final model with the test set
+# plug in the best parameter: C = 2 and gamma = 0.15
+clf = SVC(C=10, gamma=0.05)
+
+# Before we run on our test set, let's try a k-fold CV to see how it performs
+k_fold = KFold(n_splits=10, random_state=3, shuffle=True)
 scorer = make_scorer(accuracy_score)
 cv_scores = cross_val_score(clf, x_train, y_train, cv=k_fold, scoring=scorer)
-cv_scores.mean() # the average accuracy is 98.04%
+cv_scores.mean() # the k-fold cv average accuracy is 98.17%
 
+# Now, run our final model with the test set!
 clf.fit(x_train, y_train)
 predictions = clf.predict(x_test)
 accuracy_score(y_test,predictions) #accuracy: 99.05% on test set
 
-# try a different split from the data, this time only 80% training data
-x_train, x_test, y_train, y_test = train_test_split( features, label, test_size=0.2 , random_state = 2 )
-clf = SVC(C=10, gamma = 0.055)
-k_fold = KFold(n_splits=10, random_state=2, shuffle=True)
-scorer = make_scorer(accuracy_score)
-cv_scores = cross_val_score(clf, x_train, y_train, cv=k_fold, scoring=scorer)
-cv_scores.mean() # the average accuracy is 98.11%
-#predict the test set
-clf.fit(x_train, y_train)
-predictions = clf.predict(x_test)
-accuracy_score(y_test, predictions) #accuracy: 97.95% on test set
+# We got 99.05% accuracy on our test set. Not bad!
+# There are some potential improvement about SVM, such as trying kernel = 'linear' or 'poly'
+# but using 'rbf', we are able to generate generally decent accuracy percentage.
 
-# future improvement: try kernel = 'linear' or 'poly'
+
+
+
+
+
+# other ML algorithm to be applied
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(x_train,y_train)
