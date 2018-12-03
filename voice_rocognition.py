5f016f7ad22166fd8c2ff6ab68d957b1266caf0f
ryan-kttam <ryantam0626@gmail.com>
2018-12-02 16:54:55 -0800

    clean the code

--- voice_rocognition.py
+++ voice_rocognition.py
@@ -10,10 +10,18 @@
 # Version 1
 
 # 1. data exploration
-import numpy as np, pandas as pd, matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import  accuracy_score , make_scorer
+from sklearn.svm import SVC
 
 # import the data
-data = pd.read_csv("voice.csv")
+data = pd.read_csv("C:/Github/Voice-Recognition-Project/voice.csv")
 
 # take a look at the data and its summary
 data.head()
@@ -27,14 +35,13 @@ data.isnull().sum()
 data['label'].value_counts() # there are 1584 male voices and 1584 female voices.
 
 # visualize features relationship
-import seaborn
 seaborn.pairplot(data[['meanfreq', 'centroid', 'IQR', 'label']], hue = 'label', size = 2)
 
 # get the column names, if interested
 list(data)
 
 # replace male as 1 and female as 0
-data = data.replace( {'label': {'male': 1, 'female' :0}} )
+data = data.replace({'label': {'male': 1, 'female' :0}})
 
 # separating features and the label
 label = data['label']
@@ -46,7 +53,7 @@ features.head()
 # Standardizing the data allows our training model to treat each features equally.
 # standardizing the data using StandardScaler
 # each attribute will have mean of 0 and sd of 1
-from sklearn.preprocessing import StandardScaler
+
 scaler = StandardScaler()
 features = scaler.fit_transform(features)
 # alternative version
@@ -54,13 +61,11 @@ features = scaler.fit_transform(features)
 # features = scaler.transform(features)
 
 # split the data into training set and test set
-from sklearn.model_selection import train_test_split
+
 x_train, x_test, y_train, y_test = train_test_split(features, label, test_size = 0.1, random_state = 1 )
 
 # 3. Model training using Support Vector Machine with default parameters
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.metrics import  accuracy_score , make_scorer
-from sklearn.svm import SVC
+
 clf = SVC()
 clf.fit(x_train, y_train)
 predictions = clf.predict(x_test)
@@ -82,12 +87,16 @@ for c in C_range:
    c_tuning_score.append(cv_scores.mean())
 
 # visualize and observe which 'C' give us the best accuracy
-import matplotlib.pyplot as plt
 plt.plot(range(1, 100), c_tuning_score)
-plt.ylabel('cross-validated accuracy')
-plt.xlabel('parameter: C')
+plt.ylabel('Cross-Validated Accuracy')
+plt.xlabel('Parameter: C')
+plt.title('10-Fold Cross-Validated Accuracy (C: 0 - 100)', size=15)
 # The maximum for C is around between 5 to 15,
 plt.plot(range(5, 15), c_tuning_score[5:15])
+plt.ylabel('Cross-Validated Accuracy')
+plt.xlabel('Parameter: C')
+plt.title('10-Fold Cross-Validated Accuracy (C: 5 - 15)', size=15)
+
 # in the above plot we can see that the maximum of C is from 9 to 11.
 # Let's break down the 10 and explore the best value for C.
 C_range2 = list ( np.arange(9.00, 11, 0.01) )
@@ -104,7 +113,7 @@ plt.plot(np.arange(9.00, 11.00, 0.01), c_tuning_score2)
 g_range = [0.0001, 0.001, .01, .1, 1, 10, 100]
 g_tuning_score = []
 for g in g_range:
-   model = SVC(gamma= g, C=10)
+   model = SVC(gamma= g, C=9)
    cv_scores = cross_val_score(model, x_train, y_train, cv=k_fold, scoring=scorer)
    g_tuning_score.append(cv_scores.mean())
 plt.plot(g_range, g_tuning_score)
@@ -124,7 +133,7 @@ plt.plot(g_range2, g_tuning_score2)
 
 # 5. Test the final model with the test set
 # plug in the best parameter: C = 2 and gamma = 0.15
-clf = SVC(C=10, gamma=0.05)
+clf = SVC(C=9, gamma=0.05)
 
 # Before we run on our test set, let's try a k-fold CV to see how it performs
 k_fold = KFold(n_splits=10, random_state=3, shuffle=True)
